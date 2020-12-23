b9196c1867ed2db5ae2f3ea6a84443fc9c4723da
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-23 09:51:39 +0530

    Init!

---
+++ git.ps1
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "Init!"
+git branch -M master
+git remote add origin https://github.com/fast-and-curious-1910/Wyern.git
+git push -u origin master
+                
